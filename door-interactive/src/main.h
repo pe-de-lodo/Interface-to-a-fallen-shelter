61b983d80219bf86b71eaa04f6daf73aac8d3858
Paul Hayes <paul.hayes@entropedia.co.uk>
2024-10-14 20:36:04 +0100

    Switch to using custom FastLED for Nrf52x

--- door-interactive/src/main.h
+++ door-interactive/src/main.h
@@ -1,7 +1,7 @@
-#define LED_PIN        2 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS 16 // Popular NeoPixel ring size
+#define LED_PIN 2 
+#define NUMPIXELS 16 
 #define MOSFET_PIN 5
-#define WAKEUP_PIN 10
+#define WAKEUP_PIN 8
 #define ALARM_PIN 9
 #pragma once
 
