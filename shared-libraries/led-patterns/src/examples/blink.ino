3e64ca2c1e558931ee560e01bc5f4c75fd1530df
Paul Hayes <paul.hayes@entropedia.co.uk>
2024-10-15 10:32:41 +0100

    Working on pattern system

--- shared-libraries/led-patterns/src/examples/blink.ino
+++ shared-libraries/led-patterns/src/examples/blink.ino
@@ -1,7 +1,7 @@
 #include <FastLED>
 #include <ledpatterns.h>
 
-
+#define LED_PIN = 2
 #define NUM_LEDS = 16
 
 CRGB *leds;
@@ -11,16 +11,24 @@ PatternCanvas canvas = PatternCanvas(leds,data,NUM_LEDS);
 
 tweenData tweenData;
 
+long lastUpdateTime = 0; 
+
 CRGB blinkPatter(ledData,tweenData){
     return CHSV(0,0,0);
 }
 
 void setup()
 {
-    canvas.TransitionToPattern(,0,tweenData)
+    canvas.TransitionToPattern(blinkPatter,0,tweenData);
 }
 
 void loop()
 {
-
+    long frameDuration = 33;
+    long updateStartTime = millis();
+    canvas.Update(lastUpdateTime - updateStartTime);
+    FastLED.show();
+    lastUpdateTime = updateStartTime;
+    long elapsed = millis()-updateStartTime; 
+    delay(frameDuration-elapsed);    
 }
