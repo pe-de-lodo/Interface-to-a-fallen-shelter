38308bed6e1b8a0c8df95909c3149da8bea89d9c
João Cabral <joao@whiletrue.pt>
2024-10-15 15:56:56 +0100

    no firmware crash

--- LED-animation-test/src/main.cpp
+++ LED-animation-test/src/main.cpp
@@ -49,6 +49,7 @@ BlankPattern blankPattern;
 
 void SendSimulatorData()
 {
+    // Udp.
     //char ReplyBuffer[] = "ACK";      // a string to send back
     Udp.beginPacket("192.168.0.2", 6000);
     // Udp.write((char*)leds);
@@ -69,6 +70,11 @@ void setup()
     delay(1000);
     Serial.println("A");
     Ethernet.init (21);
+    
+    byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01 };
+    arduino::IPAddress ip = arduino::IPAddress("192.168.0.3");
+    Ethernet.begin(mac, ip);
+
     Serial.println("B");
     Udp.begin(1234);
     Serial.println("C");
